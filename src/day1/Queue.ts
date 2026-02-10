92c1aa6054c55b62241522a7b617adde47ac3f31
Himanshu Gohil <himanshugohil234@gmail.com>
2026-02-10 17:29:24 +0000

    implemented stack and queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,57 +1,54 @@
 type Node<T> = {
-    val: T,
+    val: T;
     next?: Node<T>;
-}
+};
 
 export default class Queue<T> {
     public length: number;
-    private head?: Node<T>;
-    private tail?: Node<T>;
+    head?: Node<T>;
+    tail?: Node<T>;
 
     constructor() {
         this.length = 0;
-        this.head = this.tail = undefined;
+        this.head = undefined;
+        this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        const node = { val: item } as Node<T>;
         this.length++;
-        const node = {val: item} as Node<T>;
 
         if (!this.tail) {
-            this.head = node;
-            this.tail = this.head;
+            this.head = this.tail = node;
         }
 
         this.tail.next = node;
         this.tail = node;
-
     }
+
     deque(): T | undefined {
         if (!this.head) {
             return undefined;
         }
 
         if (this.length === 1) {
-            const head = this.head;
+            const val = this.head.val;
+
             this.head = this.tail = undefined;
-
-            // NOTE: Optional: free the space; (free memory in languages like C and C++)
-            head.next = undefined;
-
             this.length--;
-            return head.val;
+            return val;
         }
 
-        const head = this.head;
+        const val = this.head.val;
         this.head = this.head.next;
-
-        // NOTE: Optional: free the space; (free memory in languages like C and C++)
         this.length--;
-        head.next = undefined;
-
-        return head.val;
+        return val;
     }
     peek(): T | undefined {
-        return this.head?.val;
+        if (!this.head) {
+            return undefined;
+        }
+
+        return this.head.val;
     }
 }
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,26 @@
 export default class Stack<T> {
     public length: number;
-
-    
+    stack: T[];
 
     constructor() {
+        this.length = 0;
+        this.stack = [];
     }
 
     push(item: T): void {
-
-}
+        this.length++;
+        this.stack.push(item);
+        return;
+    }
     pop(): T | undefined {
+        if (this.length === 0) return undefined;
 
-}
+        this.length--;
+        return this.stack.pop();
+    }
     peek(): T | undefined {
+        if (this.length === 0) return undefined;
 
-}
+        return this.stack[this.length - 1];
+    }
 }
