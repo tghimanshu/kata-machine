5f0c16ae726eafb23773b4942c63fd682c13c3e7
Himanshu Gohil <himanshugohil234@gmail.com>
2026-02-18 17:54:06 +0000

    WIP: Ringbuffer working like a ring

--- src/__tests__/RingBuffer.ts
+++ src/__tests__/RingBuffer.ts
@@ -4,20 +4,35 @@ test("RingBuffer", function() {
     const buffer = new RingBuffer<number>();
 
     buffer.push(5);
+    console.log(buffer);
     expect(buffer.pop()).toEqual(5);
+    console.log(buffer);
     expect(buffer.pop()).toEqual(undefined);
+    console.log(buffer);
 
     buffer.push(42);
+    console.log(buffer);
     buffer.push(9);
     console.log(buffer);
+    console.log(buffer);
+    console.log(buffer);
     expect(buffer.pop()).toEqual(42);
+    console.log(buffer);
     expect(buffer.pop()).toEqual(9);
+    console.log(buffer);
     expect(buffer.pop()).toEqual(undefined);
+    console.log(buffer);
 
     buffer.push(42);
+    console.log(buffer);
     buffer.push(9);
+    console.log(buffer);
     buffer.push(12);
+    console.log(buffer);
     expect(buffer.get(2)).toEqual(12);
+    console.log(buffer);
     expect(buffer.get(1)).toEqual(9);
+    console.log(buffer);
     expect(buffer.get(0)).toEqual(42);
+    console.log(buffer);
 });
--- src/day1/RingBuffer.ts
+++ src/day1/RingBuffer.ts
@@ -9,38 +9,38 @@ export default class RingBuffer<T> {
     constructor() {
         this.buffer = new Array<T>(this.size);
 
-        this.head = 0;
-        this.tail = 0;
+        this.head = 8;
+        this.tail = 8;
         this.length = 0;
     }
     push(value: T): void {
-        if (!this.buffer[this.head]) {
+        if (!this.buffer[this.head % this.size]) {
             this.length++;
-            this.buffer[this.head] = value;
+            this.buffer[this.head % this.size] = value;
             return;
         }
 
-        this.buffer[this.tail + 1] = value;
-        this.tail++;
+        this.buffer[(this.tail + 1) % this.size] = value;
+        this.tail += this.tail % this.size;
         this.length++;
         return;
     }
     pop(): T | undefined {
         if (this.length === 0) return undefined;
-        const val = this.buffer[this.head];
-        delete this.buffer[this.head];
+        const val = this.buffer[this.head % this.size];
+        delete this.buffer[this.head % this.size];
         if (this.head === this.tail) {
             this.head = 0;
             this.tail = 0;
             return val;
         }
-        this.head++;
+        this.head += (this.head + 1) % this.size;
         this.length--;
         return val;
     }
     get(index: number): T | undefined {
         if (index >= this.length) return undefined;
 
-        return this.buffer[this.head + index];
+        return this.buffer[(this.head + index) % this.size];
     }
 }
