62b936ef9d67010b43e3722f3ae20ff87deb8ebb
Himanshu Gohil <himanshugohil234@gmail.com>
2026-03-28 16:31:29 +0530

    DFSOnBST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,16 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function traverse(
+    node: BinaryNode<number> | null,
+    needle: number,
+): boolean | undefined {
+    if (node === null) return;
 
+    if (node.value === needle) return true;
+
+    if (node.value > needle) return traverse(node.left, needle);
+    if (node.value < needle) return traverse(node.right, needle);
+    return false;
 }
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return traverse(head, needle) ?? false;
+}
+
