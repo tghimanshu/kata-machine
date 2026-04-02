e0a960b7036c0557fc8b1cb9d76077ca8de620f1
Himanshu Gohil <himanshugohil234@gmail.com>
2026-04-02 23:44:06 +0530

    WIP: Maze Solver

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,7 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {}
 
-}
